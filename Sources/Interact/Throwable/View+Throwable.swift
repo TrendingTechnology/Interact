9072b99b3355a27ba80e57545ffaf7154c297fcf
Kieran Brown <kb6627500@gmail.com>
2019-11-17 19:39:13 -0500

    Keep updating documentation

--- Sources/Interact/Throwable/View+Throwable.swift
+++ Sources/Interact/Throwable/View+Throwable.swift
@@ -13,10 +13,17 @@ import SwiftUI
 public extension View {
     
     /// Use this modifier to be able to drag and throw a view.
+    ///
+    /// -   note:
+    ///     If you want to resize or rotate you view as well make sure to this modifier last in the chain.  Not doing so will have unintended effects. The order draggable and throwable modifiers will always come last.
+    ///
+    ///
     /// - parameters:
     ///
     ///     - model: A type conforming to the `VelocityModel` protocol, the default value is `Velocity()` which provides the most basic velocity animation.
     ///     - threshold: The magnitude required to throw the view upon release of the drag gesture. **default value** = 0
+    ///
+    ///
     ///
     func throwable(model: VelocityModel = Velocity(), threshold: CGFloat = 0) -> some View {
         self.modifier(ThrowableModifier(model: model, threshold: threshold))
